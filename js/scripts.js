e66bebc6b447b457923b4d122696af8f3d0775bd
Stewart ilondanga <stilondanga@gmail.com>
2017-12-21 19:05:43 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -12,11 +12,11 @@ if ( now < 07 ){
 } else if ( now < 10 ) {
   message = 'There are lots of people who mistake their imagination for their memory. Good Morning';
 } else if ( now < 11 ) {
-  message
+  message = 'If I want to knock a story off the front page, I just change my hairstyle. Good Morning';
 } else if ( now < 12 ) {
   message = 'I have a new philosophy. I am only going to dread one day at a time. Good Morning';
 } else if ( now < 13 ) {
-  message = ''
+  message = 'A committee is a group that keeps minutes and loses hours. Good Afternoon!';
 } else if ( now < 14 ) {
   message
 } else if ( now < 15 ) {

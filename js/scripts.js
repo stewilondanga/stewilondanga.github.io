4ba28d8a6de76616db776a508c2fb1efdaa16173
Stewart ilondanga <stilondanga@gmail.com>
2017-12-21 18:18:12 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,40 @@ var now = today.getHours();
 var greeting = document.getElementById("greeting");
 var message;
 
-if ( now < 12 ){
+if ( now < 07 ){
   message = 'Everyday may not be good, but there is something good in every day! Stewart ilondanga says Good Morning';
+} else if ( now < 08 ) {
+  message
+} else if ( now < 09 ) {
+  message
+} else if ( now < 10 ) {
+  message
+} else if ( now < 11 ) {
+  message
+} else if ( now < 12 ) {
+  message
+} else if ( now < 13 ) {
+  message
+} else if ( now < 14 ) {
+  message
+} else if ( now < 15 ) {
+  message
+} else if ( now < 16 ) {
+  message
+} else if ( now < 17 ) {
+  message
 } else if ( now < 18 ) {
-  message = 'An apple a day keeps the doctor away! Stewart ilondanga says Good Afternoon';
-} else if ( now < 21 ){
-  message = 'Every sunset brings the promise of a new dawn! Stewart ilondanga says Good Evening';
+  message = 'I always wanted to be somebody, but now I realize I should have been more specific. Good Afternoon!';
+} else if ( now < 19 ) {
+  message = 'Every sunset brings the promise of a new dawn! Good Evening';
+} else if ( now < 20 ) {
+  message
+} else if ( now < 21 ) {
+  message
+} else if ( now < 22 ) {
+  message
+} else if ( now < 23 ) {
+  message
 } else {
   message = 'Preparing for tommorrow? Stewart ilondanga says Good night!';
 }

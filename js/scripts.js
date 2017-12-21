c4521949403ad465f43c4e414d23d92ee969fc8d
Stewart ilondanga <stilondanga@gmail.com>
2017-12-21 10:38:26 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,13 @@ var greeting = document.getElementById("greeting");
 var message;
 
 if ( now < 12 ){
-  message = 'Welcome! Stewart ilondanga says Good Morning';
+  message = 'Whether tea or coffee, a cup will keep you warm! Stewart ilondanga says Good Morning';
 } else if ( now < 18 ) {
-  message = 'Welcome! Stewart ilondanga says Good Afternoon';
+  message = 'An apple a day keeps the doctor away! Stewart ilondanga says Good Afternoon';
+} else if ( now < 21 ){
+  message = 'Enjoying the review! Stewart ilondanga says Good Evening';
 } else {
-  message = 'Welcome! Stewart ilondanga says Good Evening';
+  message = 'Preparing for tommorrow? Stewart ilondanga says Good night!';
 }
 
 greeting.innerHTML = message;

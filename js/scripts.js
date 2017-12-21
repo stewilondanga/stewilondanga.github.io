3b1be8f280c6d27584045d7d12b514c60770b134
Stewart ilondanga <stilondanga@gmail.com>
2017-12-21 14:39:47 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -38,3 +38,12 @@ $(document).ready(function(){
 	});
 
 });
+
+var navigate = (function() {
+	$('.dd').toggle();
+	$('.dd_btn').click(function() {
+		var dataName = $(this).attr('data-name');
+		$('.dd').hide();
+		$('.' + dataName).toggle();
+	});
+})();

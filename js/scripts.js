b29c0bd660f56c38df0e9a5a4179010451bd1d12
Stewart ilondanga <stilondanga@gmail.com>
2017-12-21 18:35:19 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@ var greeting = document.getElementById("greeting");
 var message;
 
 if ( now < 07 ){
-  message = 'Everyday may not be good, but there is something good in every day! Good Morning';
-} else if ( now < 08 ) {
   message = 'Laugh and the world laughs with you, snore and you sleep alone! Good Morning';
+} else if ( now < 08 ) {
+  message = 'Everyday may not be good, but there is something good in every day! Good Morning';
 } else if ( now < 09 ) {
   message
 } else if ( now < 10 ) {
